161b474dc41a5130fa484a32f40bd4c0d415f7c9
anggadwiutomo <32736129+anggadwiutomo@users.noreply.github.com>
2017-10-22 18:52:04 +0700

    Tugas 2

---
+++ 2/app/src/main/java/com/example/user/aplikasipenampilkodesumberwebsite/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.user.aplikasipenampilkodesumberwebsite;
+
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv = (TextView) findViewById(R.id.textView);
+
+        tv.setText(GetWebsite("https://www.google.co.id/"));
+    }
+
+    public String GetWebsite(String website) {
+        String resString = "";
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpGet httpget = new HttpGet(website);
+        try {
+            HttpResponse response;
+            response = httpclient.execute(httpget);
+            HttpEntity entity = response.getEntity();
+            InputStream is = entity.getContent();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, "windows-1251"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            resString = sb.toString();
+            is.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
+        }
+        return resString;
+    }
+}
---
+++ 2/app/src/test/java/com/example/user/aplikasipenampilkodesumberwebsite/MainActivityTest.java
@@ -0,0 +1,10 @@
+package com.example.user.aplikasipenampilkodesumberwebsite;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by User on 22/10/2017.
+ */
+public class MainActivityTest {
+
+}
